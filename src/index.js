abbea373513768efd5ff445257aae0fdaa8ed2a3
Kier Borromeo <seraphipod@gmail.com>
2017-12-16 16:53:19 +0800

    Add hooks example

--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
 export {default as confirm} from './confirm';
 export {default as ConfirmRoot} from './ConfirmRoot';
-export {default as ConfirmNode} from './ConfirmNode';
--- storybook/confirm.js
+++ storybook/confirm.js
@@ -44,4 +44,60 @@ storiesOf('Confirm', module)
 
     return <Basic />
   })
+  .add('hooks', () => {
+    class Basic extends React.Component {
+      render() {
+        return (
+          <div>
+            <ConfirmRoot>
+              {({active, text, actions, options}) => (
+                <div>
+                  <Modal isOpen={active} onRequestClose={actions.dismiss}>
+                    {text}
+                    <button onClick={actions.proceed}>Proceed</button>
+                    <button onClick={actions.dismiss}>Dismiss</button>
+                  </Modal>
+                </div>
+              )}
+            </ConfirmRoot>
+
+            <SomewhereDeepDownInTheVDOMTree />
+          </div>
+        )
+      }
+    }
+
+    class SomewhereDeepDownInTheVDOMTree extends React.Component {
+      state = {
+        confirming: false
+      }
+
+      render() {
+        return (
+          <div>
+            <ConfirmNode onOpen={this.handleOpen} onClose={this.handleClose} />
+
+            <button onClick={this.handleClick}>
+              Open Confirmation
+            </button>
+          </div>
+        )
+      }
+
+      handleClick() {
+        this.setState({ confirming: true })
+
+        confirm(`You haven't finished your post yet. Do you want to leave without finishing?`)
+          .then(() => {
+            console.log('Proceed')
+            this.setState({ confirming: false })
+          }, () => {
+            console.log('Dismissed')
+            this.setState({ confirming: false })
+          })
+      }
+    }
+
+    return <SomewhereDeepDownInTheVDOMTree />
+  })
   
